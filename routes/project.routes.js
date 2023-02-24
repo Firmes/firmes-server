6275abe60deead6e0ffef1c6d6fe809cbd664402
jt-96 <jetorrico@outlook.com>
2023-02-24 16:13:24 -0300

    fix(server): fix update project method, fix query, add comments

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -45,14 +45,15 @@ router.post('/create-new-project', async (req, res) => {
     try {
         const { project_title, project_client, project_description, project_year, project_videoURL, project_type } = req.body.projectDetails;
         const { projectImages } = req.body;
-        
+
+        //Error with projectTypeId, all the other values are saved correctly
         const projectTypeId = getTypeId(project_type, pool);
         const project_info = await pool
-        .query(`INSERT INTO firmes.project_info (project_title, project_client, project_description, project_creation_year, project_video_url, project_type) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`, [project_title, project_client, project_description, Number(project_year), project_videoURL, projectTypeId]);
+            .query(`INSERT INTO firmes.project_info (project_title, project_client, project_description, project_creation_year, project_video_url, project_type) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`, [project_title, project_client, project_description, Number(project_year), project_videoURL, projectTypeId]);
 
-        for (let imageInfo of projectImages ) {
+        for (let imageInfo of projectImages) {
             await pool
-                .query(`INSERT INTO firmes.project_image_url (project_info_id_fk, project_image_url, image_is_portrait) VALUES ($1, $2, $3)`, [project_info.rows[0].project_info_id, imageInfo.imageUrl, imageInfo.image_is_portrait]);
+                .query(`INSERT INTO firmes.project_image_url (project_info_id_fk, project_image_url, image_is_portrait) VALUES ($1, $2, $3)`, [project_info.rows[0].project_info_id, imageInfo.project_image_url, imageInfo.image_is_portrait]);
         }
 
         return res.status(200).send(project_info.rows[0]);
@@ -66,14 +67,20 @@ router.post('/create-new-project', async (req, res) => {
 router.put('/edit-project/:projectId', async (req, res) => {
     try {
         const { projectId } = req.params;
-        const { title, client, description, creation_year, video_url, type, images } = req.body;
-        const project_info = await pool
-            .query(`UPDATE firmes.project_info SET (project_title, project_client, project_description, project_creation_year, project_video_url, project_type) WHERE project_id = ${projectId} RETURNING *`, [title, client, description, creation_year, video_url, type]);
+        const { project_title, project_client, project_description, project_year, project_videoURL, project_type } = req.body.projectDetails;
+        const { projectImages } = req.body;
 
-        for (let imageInfo of images) {
+        //Check project type, add existing type check?, all the other values are saved correctly
+        const project_info = await pool
+            .query(`UPDATE firmes.project_info SET project_title = '${project_title}', project_client = '${project_client}', project_description = '${project_description}', project_creation_year = ${Number(project_year)}, project_video_url = '${project_videoURL}', project_type = ${project_type} WHERE project_info_id = ${projectId} RETURNING *`);
+
+        for (let imageInfo of projectImages) {
             await pool
-                .query(`UPDATE firmes.project_image_url SET (project_info_id_fk, project_image_url) WHERE image_id = ${imageInfo.image_id}`, [project_info.rows[0].project_info_id, imageInfo.imageUrl]);
+                .query(`UPDATE firmes.project_image_url SET project_info_id_fk = ${project_info.rows[0].project_info_id}, project_image_url = '${imageInfo.project_image_url}', image_is_portrait = ${imageInfo.image_is_portrait} WHERE image_id = ${imageInfo.image_id}`);
         }
+
+        return res.status(200).json({message: 'Project updated'});
+
     } catch (error) {
         console.log(error)
         return res.status(400).json({ errorMessage: 'An internal error just occurred' });

2e83c3c876e2a139d0f168083c8921cb1ecfe313
Zabdiel Segura <zabdiel-segura@hotmail.com>
2023-03-03 15:02:46 -0600

    Create routes to save and show contacts info

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ app.use('/project', projectRoutes)
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+const contactRoutes = require('./routes/contact.routes');
+app.use('/contact', contactRoutes)
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 

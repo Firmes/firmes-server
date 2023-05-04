0b94edaa4bfcd326e275ee9b43b92ce82a95acc3
rodridega <rodridega@gmail.com>
2023-05-04 11:49:08 -0300

    fix origin

--- config/index.js
+++ config/index.js
@@ -21,7 +21,6 @@ module.exports = (app) => {
   // Services like Fly use something called a proxy and you need to add this to your server
   app.set("trust proxy", 1);
 
-  const FRONTEND_URL = "http://localhost:5173" || process.env.ORIGIN
 
   // controls a very specific header to pass headers from the frontend
   app.use(
